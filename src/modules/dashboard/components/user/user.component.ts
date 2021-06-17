f923e62862f2f2472c6060714b2e7bb1be348bd5
unknown <hunganh250590@gmail.com>
2021-06-17 11:30:59 +0700

    heroku hoàn thành

--- src/modules/dashboard/components/user/user.component.ts
+++ src/modules/dashboard/components/user/user.component.ts
@@ -125,8 +125,6 @@ export class UserComponent implements OnInit {
         }).then(result => {
             if (result.value) {
                 this.dashboardService.deleteDatSan(datsan.id).subscribe(data => {
-                    console.log(data);
-                    
                     if (data.status) {
                         Swal.fire({
                             icon: 'success',
@@ -135,6 +133,12 @@ export class UserComponent implements OnInit {
                             timer: 1500
                         });       
                         this.getListDatSanByUserToken();
+                    }
+                    else{
+                        Swal.fire({
+                            icon: 'error',
+                            text: data.message,
+                        })
                     }
                 });
             }
