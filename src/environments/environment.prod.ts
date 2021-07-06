389c993d7ebb34c63335dba42cdc12a97c7d669a
unknown <hunganh250590@gmail.com>
2021-07-06 16:05:55 +0700

    heroku hoàn thành 5

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
     production: true,
-    url: 'http://localhost:8000',
+//    url: 'http://localhost:8000',
 
-    // url: 'https://backenddatsanbong.herokuapp.com',
+    url: 'https://backenddatsanbong.herokuapp.com',
     // url:'https://dashboard.heroku.com/apps/backenddatsanbong',
-    // urlCLU: 'https://webdatsandabanh.herokuapp.com',
-    urlCLU: 'http://localhost:4200',
+     urlCLU: 'https://frontenddatsanbong.herokuapp.com',
+    //urlCLU: 'http://localhost:4200',
 
 
 };
